02252411d05261a4b5cd87482dc05c2a64c8a2b7
Jhon <jhoncardenasf07@gmail.com>
2025-05-27 23:42:26 -0500

    Se renombraron los paquetes "model" y "view" a "server" y "client" respectivamente, esto ya que es más fácil entender la lógica para los sockets de este modo. También se repartieron las clases dentro del paquete "utils" entre el "server" y el "client", simplemente se dejo el "ImgManager" en el "client" y de resto todo en el server. Se movio la clase "Main" al server, para dejar como nivel inicial unicamente los paquetes "server" y "client".

---
+++ src/main/java/co/edu/uptc/client/game/CardsPanel.java
@@ -0,0 +1,60 @@
+package co.edu.uptc.client.game;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import co.edu.uptc.client.game.draw.CardImage;
+
+public class CardsPanel extends JPanel {
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private ArrayList<CardImage> cards = new ArrayList<>();
+
+    public CardsPanel(Dimension dimension) {
+        setBorder(BorderFactory.createLineBorder(Color.RED));
+        setLayout(new GridBagLayout());
+        setOpaque(false);
+        setPreferredSize(dimension);
+        setAlignmentY(Component.TOP_ALIGNMENT);
+    }
+
+    public void addCard(int row, int column) {
+        CardImage label = new CardImage(row, column);
+        cards.add(label);
+
+        if (cards.size() > 3) {
+            removeAll();
+            repaintMultipleCards();
+        } else {
+            gbc.ipadx = 5;
+            gbc.ipady = 5;
+            add(label, gbc);
+            revalidate();
+            repaint();
+        }
+    }
+
+    private void repaintMultipleCards() {
+        for (int i = 0; i < cards.size(); i++) {
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.insets = new Insets(0, (cards.size()*20)-(i*20) , 0, i*30);
+
+            add(cards.get(i), gbc);
+        }
+    }
+
+    public void clearCards() {
+        cards.clear();
+        removeAll();
+        revalidate();
+        repaint();
+    }
+}
